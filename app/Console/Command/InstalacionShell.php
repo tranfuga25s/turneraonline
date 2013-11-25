fe0be402c31b0272710962a68cb98c44d1799b2a
tranfuga25s <tranfuga25s@gmail.com>
2013-11-25 14:13:38 -0300

    Agregada reinicialización de datos para la demostración - issue #99

--- app/Console/Command/InstalacionShell.php
+++ app/Console/Command/InstalacionShell.php
@@ -1,12 +1,22 @@
 <?php
 
+App::uses( 'AuthComponent', 'Controller/Component' );
+
 class InstalacionShell extends AppShell {
 
-	var $uses = array( 'Usuario', 'Clinica', 'Consultorio', 'Medico', 'Secretaria' );
+	public $uses = array( 'Usuario',
+	                      'Clinica',
+	                      'Consultorio',
+	                      'Medico',
+	                      'Secretaria',
+	                      'Especialidad',
+	                      'Disponibilidad',
+	                      'DiaDisponibilidad',
+	                      'Turno' );
 
     private $clinica =  array(
         'Clinica' => array(
-                'id_clinica' => '1',
+                'id_clinica' => 1,
                 'nombre' => 'Clinica de Prueba',
                 'direccion' => 'Dirección de prueba',
                 'telefono' => '2147483647',
@@ -19,124 +29,238 @@ class InstalacionShell extends AppShell {
     );
 
     private $consultorios = array(
-        'Consultorios' => array(
-            'id_consultorio' => '1',
-            'clinica_id' => '1',
-            'nombre' => 'Consultorio 1'
-         ),
-         'Consultorios' => array(
-            'id_consultorio' => '2',
-            'clinica_id' => '1',
-            'nombre' => 'Consultorio 2'
-         )
+        0 => array(
+            'Consultorio' => array(
+                'id_consultorio' => 1,
+                'clinica_id' => 1,
+                'nombre' => 'Consultorio 1'
+             )
+        ),
+        1 => array(
+            'Consultorio' => array(
+                'id_consultorio' => 2,
+                'clinica_id' => 1,
+                'nombre' => 'Consultorio 2'
+             )
+        )
     );
 
     private $medicos = array(
-        'Medicos' => array(
-            'id_medico' => '1',
-            'usuario_id' => '17',
-            'especialidad_id' => '1',
-            'clinica_id' => '1',
-            'visible' => true
+        0 => array(
+            'Medico' => array(
+                'id_medico' => 1,
+                'usuario_id' => 17,
+                'especialidad_id' => 1,
+                'clinica_id' => 1,
+                'visible' => true
+            )
         ),
-        'Medicos' => array(
-            'id_medico' => '2',
-            'usuario_id' => '6',
-            'especialidad_id' => '2',
-            'clinica_id' => '1',
-            'visible' => true
-       )
+        1 => array(
+            'Medico' => array(
+                'id_medico' => 2,
+                'usuario_id' => 6,
+                'especialidad_id' => 2,
+                'clinica_id' => 1,
+                'visible' => true
+           )
+        )
     );
 
     private $secretarias = array(
-        'Secretaria' => array(
-            'id_secretaria' => '2',
-            'usuario_id' => '7',
-            'clinica_id' => '1',
-            'resumen' => true
+        0 => array(
+            'Secretaria' => array(
+                'id_secretaria' => 2,
+                'usuario_id' => 7,
+                'clinica_id' => 1,
+                'resumen' => true
+            )
         ),
-        'Secretaria' => array(
-            'id_secretaria' => '7',
-            'usuario_id' => '18',
-            'clinica_id' => '1',
-            'resumen' => true
+        1 => array(
+            'Secretaria' => array(
+                'id_secretaria' => 7,
+                'usuario_id' => 18,
+                'clinica_id' => 1,
+                'resumen' => true
+            )
         )
     );
 
     private $usuarios = array(
+        0 => array(
         'Usuario' => array(
-                'id_usuario' => '2',
+                'id_usuario' => 2,
                 'email' => 'tranfuga25s@gmail.com',
                 'nombre' => 'Esteban Javier',
                 'apellido' => 'Zeller',
                 'telefono' => '2313',
                 'celular' => '3424293436',
-                'obra_social_id' => '6',
+                'obra_social_id' => 6,
                 'notificaciones' => true,
-                'contra' => '999d9cffdf4589ff0b2c5cc951fd7d8b872ade69',
-                'grupo_id' => '1',
+                'contra' => 'admin',
+                'grupo_id' => 1,
                 'facebook_id' => '0',
                 'sexo' => 'm'
-        ),
+        ) ),
+        1 => array (
         'Usuario' => array(
-                'id_usuario' => '5',
+                'id_usuario' => 5,
                 'email' => 'admin@admin.com.ar',
                 'nombre' => 'Administrador',
                 'apellido' => 'Test',
                 'telefono' => '0293',
                 'celular' => '0349',
-                'obra_social_id' => '5',
+                'obra_social_id' => 5,
                 'notificaciones' => true,
-                'contra' => 'be310bfe533c9e09f9967523e122519dc9057915',
-                'grupo_id' => '1',
+                'contra' => 'admin',
+                'grupo_id' => 1,
                 'facebook_id' => '0',
                 'sexo' => 'm'
-        ),
+        ) ),
+        2 => array(
         'Usuario' => array(
-                'id_usuario' => '17',
+                'id_usuario' => 17,
                 'email' => 'medico@turnera.com',
                 'nombre' => 'Medico',
                 'apellido' => 'Medico',
                 'telefono' => '',
                 'celular' => '',
-                'obra_social_id' => '5',
+                'obra_social_id' => 5,
                 'notificaciones' => true,
-                'contra' => '5dd905b984ff7f99ff5e6a7ad1d8cc99419c6161',
-                'grupo_id' => '2',
+                'contra' => 'medico',
+                'grupo_id' => 2,
                 'facebook_id' => '0',
                 'sexo' => 'm'
-        ),
+        ) ),
+        3 => array(
         'Usuario' => array(
-                'id_usuario' => '18',
+                'id_usuario' => 18,
                 'email' => 'secretaria@turnera.com',
                 'nombre' => 'Secretaria',
                 'apellido' => 'Secretaria',
                 'telefono' => '',
                 'celular' => '',
-                'obra_social_id' => '5',
+                'obra_social_id' => 5,
                 'notificaciones' => true,
-                'contra' => 'fee2e7a4ecedf70cb24bfc0341ee7452e3e9a030',
-                'grupo_id' => '3',
+                'contra' => 'secretaria',
+                'grupo_id' => 3,
                 'facebook_id' => '0',
                 'sexo' => 'f'
-        ),
+        ) ),
+        4 => array(
         'Usuario' => array(
-                'id_usuario' => '19',
+                'id_usuario' => 19,
                 'email' => 'paciente@turnera.com',
                 'nombre' => 'Paciente',
                 'apellido' => 'Paciente',
                 'telefono' => '',
                 'celular' => '',
-                'obra_social_id' => '5',
+                'obra_social_id' => 5,
                 'notificaciones' => true,
-                'contra' => '4be21d15e3f73357bcf094bafba7c4bef4d6bca1',
-                'grupo_id' => '4',
+                'contra' => 'paciente',
+                'grupo_id' => 4,
                 'facebook_id' => '0',
                 'sexo' => 'f'
-        )
+        ) )
+    );
+
+    private $especialidades = array(
+        0 => array(
+        'Especialidad' => array(
+            'id_especialidad' => 1,
+            'nombre' => 'Ginecología'
+        ) ),
+        1 => array(
+        'Especialidad' => array(
+            'id_especialidad' => 2,
+            'nombre' => 'Otorrinolaringologia'
+        ) )
+    );
+
+    private $disponibilidades = array(
+        0 => array(
+            'Disponibilidad' => array(
+                'id_disponibilidad' => 1,
+                'medico_id' => 1,
+                'consultorio_id' => 1,
+                'duracion' => 20,
+            ),
+            'DiaDisponibilidad' => array(
+                    array(
+                        'id' => 1,
+                        'disponibilidad_id' => 1,
+                        'dia' => 1,
+                        'habilitado' => 1,
+                        'hora_inicio' => '08:00:00',
+                        'hora_fin' => '11:00:00',
+                        'hora_inicio_tarde' => '14:00:00',
+                        'hora_fin_tarde' => '20:00:00'
+                    ),
+                    array(
+                        'id' => 2,
+                        'disponibilidad_id' => 1,
+                        'dia' => 2,
+                        'habilitado' => 1,
+                        'hora_inicio' => '08:00:00',
+                        'hora_fin' => '11:00:00',
+                        'hora_inicio_tarde' => '14:00:00',
+                        'hora_fin_tarde' => '20:00:00'
+                    ),
+                    array(
+                        'id' => 3,
+                        'disponibilidad_id' => 1,
+                        'dia' => 4,
+                        'habilitado' => 1,
+                        'hora_inicio' => '08:00:00',
+                        'hora_fin' => '11:00:00',
+                        'hora_inicio_tarde' => '14:00:00',
+                        'hora_fin_tarde' => '20:00:00'
+                    ),
+            )
+       ),
+       1 => array(
+           'Disponibilidad' => array(
+                'id_disponibilidad' => 2,
+                'medico_id' => 2,
+                'consultorio_id' => 2,
+                'duracion' => 30
+           ),
+           'DiaDisponibilidad' => array(
+                    array(
+                        'id' => 4,
+                        'disponibilidad_id' => 2,
+                        'dia' => 1,
+                        'habilitado' => 1,
+                        'hora_inicio' => '08:00:00',
+                        'hora_fin' => '11:00:00',
+                        'hora_inicio_tarde' => '14:00:00',
+                        'hora_fin_tarde' => '20:00:00'
+                    ),
+                    array(
+                        'id' => 5,
+                        'disponibilidad_id' => 2,
+                        'dia' => 2,
+                        'habilitado' => 1,
+                        'hora_inicio' => '08:00:00',
+                        'hora_fin' => '11:00:00',
+                        'hora_inicio_tarde' => '14:00:00',
+                        'hora_fin_tarde' => '20:00:00'
+                    ),
+                    array(
+                        'id' => 6,
+                        'disponibilidad_id' => 2,
+                        'dia' => 4,
+                        'habilitado' => 1,
+                        'hora_inicio' => '08:00:00',
+                        'hora_fin' => '11:00:00',
+                        'hora_inicio_tarde' => '14:00:00',
+                        'hora_fin_tarde' => '20:00:00'
+                    ),
+           )
+       )
     );
 
     public function solo_datos() {
+
         $this->out( 'Sistema de reinistalacion de datos', 1, Shell::NORMAL  );
         // Los parametros están en $this->params
         $dataSource = $this->Clinica->getDataSource();
@@ -150,8 +274,8 @@ class InstalacionShell extends AppShell {
             $dataSource->rollback();
             return false;
         }
-        if( $this->Clinica->save( $this->clinica, false ) ) {
-            $this->out( '<info>Clinica Guardada<info>', 1, Shell::NORMAL  );
+        if( $this->Clinica->saveMany( $this->clinica, array( 'validate' => false )  ) ) {
+            $this->out( '<info>Clinicas Guardadas</info>', 1, Shell::NORMAL  );
        } else {
             $this->out( '<error>No se pudo guardar la clinica</error>', 1, Shell::QUIET );
             $dataSource->rollback();
@@ -167,8 +291,8 @@ class InstalacionShell extends AppShell {
             $dataSource->rollback();
             return false;
         }
-        if( $this->Consultorio->save( $this->consultorios, false ) ) {
-            $this->out( '<info>Consultorios Guardados<info>', 1, Shell::NORMAL  );
+        if( $this->Consultorio->saveMany( $this->consultorios, array( 'validate' => false )  ) ) {
+            $this->out( '<info>Consultorios Guardados</info>', 1, Shell::NORMAL  );
         } else {
             $this->out( '<error>No se pudo guardar los consultorios</error>', 1, Shell::QUIET );
             $dataSource->rollback();
@@ -184,8 +308,8 @@ class InstalacionShell extends AppShell {
             $dataSource->rollback();
             return false;
         }
-        if( $this->Especialidad->save( $this->especialidades, false ) ) {
-            $this->out( '<info>Especialidades Guardados<info>', 1, Shell::NORMAL  );
+        if( $this->Especialidad->saveMany( $this->especialidades, array( 'validate' => false )  ) ) {
+            $this->out( '<info>Especialidades Guardados</info>', 1, Shell::NORMAL  );
         } else {
             $this->out( '<error>No se pudo guardar las especialidades</error>', 1, Shell::QUIET );
             $dataSource->rollback();
@@ -203,8 +327,8 @@ class InstalacionShell extends AppShell {
             $dataSource->rollback();
             return false;
         }
-        if( $this->Usuario->save( $this->usuarios, false ) ) {
-            $this->out( '<info>Usuarios Guardados<info>', 1, Shell::NORMAL  );
+        if( $this->Usuario->saveMany( $this->usuarios, array( 'validate' => false )  ) ) {
+            $this->out( '<info>Usuarios Guardados</info>', 1, Shell::NORMAL  );
         } else {
             $this->out( '<error>No se pudo guardar los usuarios</error>', 1, Shell::QUIET );
             $dataSource->rollback();
@@ -220,8 +344,8 @@ class InstalacionShell extends AppShell {
             $dataSource->rollback();
             return false;
         }
-        if( $this->Medico->save( $this->medicos, false ) ) {
-            $this->out( '<info>Medicos Guardados<info>', 1, Shell::NORMAL  );
+        if( $this->Medico->saveMany( $this->medicos, array( 'validate' => false ) ) ) {
+            $this->out( '<info>Medicos Guardados</info>', 1, Shell::NORMAL  );
         } else {
             $this->out( '<error>No se pudo guardar los medicos</error>', 1, Shell::QUIET );
             $dataSource->rollback();
@@ -237,10 +361,35 @@ class InstalacionShell extends AppShell {
             $dataSource->rollback();
             return false;
         }
-        if( $this->Secretaria->save( $this->secretarias, false ) ) {
-            $this->out( '<info>Secretarias Guardados<info>', 1, Shell::NORMAL );
+        if( $this->Secretaria->saveMany( $this->secretarias, array( 'validate' => false )  ) ) {
+            $this->out( '<info>Secretarias Guardados</info>', 1, Shell::NORMAL );
         } else {
             $this->out( '<error>No se pudo guardar los Secretarias</error>', 1, Shell::QUIET );
+            $dataSource->rollback();
+            return false;
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////
+        // configuro la disponibilidad
+        $this->out( '<info>Reiniciando disponibilidades</info>', 1, Shell::NORMAL  );
+        if( $this->Disponibilidad->deleteAll( array( '1=1' ), true, false ) ) {
+            $this->out( '<info>Disponibilidades eliminadas</info>', 1, Shell::NORMAL  );
+        } else {
+            $this->out( '<error>No se pudo eliminar las disponibilidades anteriores</error>', 1, Shell::QUIET );
+            $dataSource->rollback();
+            return false;
+        }
+        if( $this->DiaDisponibilidad->deleteAll( array( '1=1' ), true, false ) ) {
+            $this->out( '<info>Dia de Disponibilidades eliminadas</info>', 1, Shell::NORMAL  );
+        } else {
+            $this->out( '<error>No se pudo eliminar los dias de disponibilidad anteriores</error>', 1, Shell::QUIET );
+            $dataSource->rollback();
+            return false;
+        }
+        if( $this->Disponibilidad->saveAssociated( $this->disponibilidades[0], array( 'validate' => false ) ) &&
+            $this->Disponibilidad->saveAssociated( $this->disponibilidades[1], array( 'validate' => false ) ) ) {
+            $this->out( '<info>Disponibilidades Guardados</info>', 1, Shell::NORMAL );
+        } else {
+            $this->out( '<error>No se pudo guardar las disponibilidades</error>', 1, Shell::QUIET );
             $dataSource->rollback();
             return false;
         }
@@ -248,6 +397,16 @@ class InstalacionShell extends AppShell {
         if( $dataSource->commit() ) {
             $this->out( '<info>Commit correcto</info>', 1, Shell::QUIET );
         }
+        // Regenero los turnos
+        $this->out( '<info>Reiniciando turnos</info>', 1, Shell::NORMAL  );
+        if( $this->Turno->deleteAll( array( '1=1' ), false, false ) ) {
+            $this->out( '<info>Turnos eliminados</info>', 1, Shell::NORMAL  );
+        } else {
+            $this->out( '<error>No se pudo eliminar los turnos anteriores</error>', 1, Shell::QUIET );
+            $dataSource->rollback();
+            return false;
+        }
+        $this->dispatchShell( 'turnos generarTurnos' );
         return true;
     }
 
