4cb68d42c50bd66545e6bf2635fa74820d3ed2b2
tranfuga25s <tranfuga25s@gmail.com>
2013-12-26 14:06:24 -0300

    Mejoras de tests

--- app/Test/Case/Controller/ControllersTest.php
+++ app/Test/Case/Controller/ControllersTest.php
@@ -10,7 +10,7 @@ class ControllersTests extends PHPUnit_Framework_TestSuite {
      * @return $suite
      */
 	public static function suite() {
-		$suite = new PHPUnit_Framework_TestSuite('All Controller Tests');
+		$suite = new PHPUnit_Framework_TestSuite('Test de controladores');
         $path = APP_TEST_CASES . DS . 'Controller' . DS;
         $suite->addTestFile( $path.'AvisosControllerTest.php' );
         $suite->addTestFile( $path.'TurnosControllerTest.php' );
