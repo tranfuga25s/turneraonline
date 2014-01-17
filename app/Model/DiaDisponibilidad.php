e8600bf7ef3e753cff54a2b6390aaa5a269b0b20
tranfuga25s <tranfuga25s@gmail.com>
2014-01-17 18:28:04 -0300

    Agregados test para issue #42, issue #43, issue #44

--- app/Model/DiaDisponibilidad.php
+++ app/Model/DiaDisponibilidad.php
@@ -8,14 +8,103 @@ App::uses('AppModel', 'Model');
 class DiaDisponibilidad extends AppModel {
 
 	public $useTable = 'dia_disponibilidad';
+
+    public $primaryKey = 'id';
+
 	public $actAs = array( 'AuditLog.Auditable' );
+
 	public $belongsTo = array(
 		'Disponibilidad' => array(
 			'className' => 'Disponibilidad',
 			'foreignKey' => 'disponibilidad_id'
 		)
 	);
-	
+
+    public $validate = array(
+        'hora_inicio' => array(
+            'formato' => array(
+                'rule' => '/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/',
+                'message' => 'Formato del campo incorrecto'
+            ),
+            'horario' => array(
+                'rule' => array( 'horarioMenorQue', 'hora_fin' ),
+                'message' => "El horario de inicio debe ser menor que el horario de fin"
+            )
+        ),
+        'hora_fin' => array(
+            'formato' => array(
+                'rule' => '/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/',
+                'message' => 'Formato del campo incorrecto'
+            ),
+            'horario' => array(
+                'rule' => array( 'horarioMayorQue', 'hora_inicio' ),
+                'message' => "El horario de inicio debe ser mayor que el horario de fin"
+            )
+        ),
+        'hora_inicio_tarde' => array(
+            'formato' => array(
+                'rule' => '/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/',
+                'message' => 'Formato del campo incorrecto'
+            ),
+            'horario' => array(
+                'rule' => array( 'horarioMenorQue', 'hora_fin_tarde' ),
+                'message' => "El horario de inicio debe ser menor que el horario de fin"
+            )
+        ),
+        'hora_fin_tarde' => array(
+            'formato' => array(
+                'rule' => '/^[0-9]{2}:[0-9]{2}:[0-9]{2}$/',
+                'message' => 'Formato del campo incorrecto'
+            ),
+            'horario' => array(
+                'rule' => array( 'horarioMayorQue', 'hora_inicio_tarde' ),
+                'message' => "El horario de inicio debe ser mayor que el horario de fin"
+            )
+        )
+    );
+
+    public function horarioMenorQue( $valor, $dato ) {
+        if( !empty( $this->data ) ) {
+            $fecha = new DateTime( 'now' );
+            if( is_array( $valor ) ) {
+                $valor = array_pop( $valor );
+            }
+            $temp = split( ":", $valor );
+            $fecha->setTime( $temp[0], $temp[1], $temp[2] );
+            if( array_key_exists( $dato, $this->data[$this->name] ) ) {
+                $fecha2 = clone $fecha;
+                $temp = split( ":", $this->data[$this->name][$dato] );
+                $fecha2->setTime( $temp[0], $temp[1], $temp[2] );
+                $diferencia = $fecha->diff( $fecha2 )->format( 's' );
+                if( $diferencia >= 0 ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function horarioMayorQue( $valor, $dato ) {
+        if( !empty( $this->data ) ) {
+            $fecha = new DateTime( 'now' );
+            if( is_array( $valor ) ) {
+                $valor = array_pop( $valor );
+            }
+            $temp = split( ":", $valor );
+            $fecha->setTime( $temp[0], $temp[1], $temp[2] );
+            if( array_key_exists( $dato, $this->data[$this->name] ) ) {
+                $fecha2 = clone $fecha;
+                $temp = split( ":", $this->data[$this->name][$dato] );
+                $fecha2->setTime( $temp[0], $temp[1], $temp[2] );
+                $diferencia = $fecha->diff( $fecha2 )->format( 's' );
+                if( $diferencia <= 0 ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 	/*!
 	 * Verifica que para las disponibilidades pasadas como parametros no exista una disponibilidad que esté durante estos días.
 	 * @param id_disponibilidad Array con las disponibildiades a verificar
@@ -29,29 +118,29 @@ class DiaDisponibilidad extends AppModel {
 		// Verifica que no haya otro turno de otra disponibilidad en el mismo horario y mismo consultorio
 		$contador = 0;
 		$contador += $this->find( 'count',
-				 		array( 'conditions' => 
+				 		array( 'conditions' =>
 				 			array( 	'id_disponibilidad' => $id_disponibilidad,
 						 			'dia' => $dia,
 						 			'TIME( hora_inicio ) <' => $hinicio,
-				 					'TIME( hora_fin ) >' => $hinicio, 
+				 					'TIME( hora_fin ) >' => $hinicio,
 								 )
 							 )
 					);
 		$contador += $this->find( 'count',
-				 		array( 'conditions' => 
+				 		array( 'conditions' =>
 				 			array( 	'id_disponibilidad' => $id_disponibilidad,
 						 			'dia' => $dia,
 						 			'TIME( hora_inicio ) <' => $hfin,
-				 					'TIME( hora_fin ) >' => $hfin, 
+				 					'TIME( hora_fin ) >' => $hfin,
 								 )
 							 )
 					);
 		$contador += $this->find( 'count',
-				 		array( 'conditions' => 
+				 		array( 'conditions' =>
 				 			array( 	'id_disponibilidad' => $id_disponibilidad,
 						 			'dia' => $dia,
 						 			'TIME( hora_inicio ) <' => $hinicio,
-				 					'TIME( hora_fin ) >' => $hfin, 
+				 					'TIME( hora_fin ) >' => $hfin,
 								 )
 							 )
 					);
--- app/Test/Case/Model/DiaDisponibilidadTest.php
+++ app/Test/Case/Model/DiaDisponibilidadTest.php
@@ -12,7 +12,9 @@ class DiaDisponibilidadTestCase extends CakeTestCase {
 	 *
 	 * @var array
 	 */
-	public $fixtures = array('app.dia_disponibilidad', 'app.disponibilidad', 'app.medico', 'app.usuario', 'app.obra_social', 'app.grupo', 'app.usuarios', 'app.especialidad', 'app.clinica', 'app.consultorios', 'app.medicos', 'app.turno', 'app.consultorio', 'app.excepcion', 'app.dias_disponibilidad');
+	public $fixtures = array(
+	   'app.dia_disponibilidad',
+	   'app.disponibilidad');
 
 	/**
 	 * setUp method
@@ -35,5 +37,36 @@ class DiaDisponibilidadTestCase extends CakeTestCase {
 
 		parent::tearDown();
 	}
+
+    public function testHorarios() {
+        $datos = array(
+            'DiaDisponibilidad' => array(
+                'dia' => 1,
+                'habilitado' => true,
+                'hora_inicio' => '00:00:00',
+                'hora_fin' => '00:00:00',
+                'hora_inicio_tarde' => '00:00:00',
+                'hora_fin_tarde' => '00:00:00'
+            )
+        );
+        $resultado = $this->DiaDisponibilidad->save( $datos );
+        $this->assertEqual( $resultado, false, "No se debe poder guardar un horario de inicio mayor al de fin" );
+        $datos['DiaDisponibilidad']['hora_inicio'] = '01:00:00';
+        $datos['DiaDisponibilidad']['hora_fin'] = '02:00:00';
+        $this->assertEqual( $this->DiaDisponibilidad->save( $datos ), true, "horarios mañana correctos y tarde nulos - no debería de fallar" );
+        $datos['DiaDisponibilidad']['hora_inicio_tarde'] = '01:30:00';
+        $this->assertEqual( $this->DiaDisponibilidad->save( $datos ), false, "horarios mañana correctos y tarde incorrecto - debería de fallar" );
+        $datos['DiaDisponibilidad']['hora_inicio_tarde'] = '03:00:00';
+        $datos['DiaDisponibilidad']['hora_fin_tarde'] = '04:00:00';
+        $this->assertEqual( $this->DiaDisponibilidad->save( $datos ), true, "horarios mañana correctos y tarde correcto - no debería de fallar" );
+    }
+
+    public function testValidacionFormatos() {
+        $this->DiaDisponibilidad->id = 1;
+        $this->assertEqual( $this->DiaDisponibilidad->saveField( 'hora_inicio', 'sdoidsosio' ), false, "No anduvo la regla de validacion de formato para hora_inicio" );
+        $this->assertEqual( $this->DiaDisponibilidad->saveField( 'hora_fin', 'sdoidsosio' ), false, "No anduvo la regla de validacion de formato para hora_fin" );
+        $this->assertEqual( $this->DiaDisponibilidad->saveField( 'hora_inicio_tarde', 'sdoidsosio' ), false, "No anduvo la regla de validacion de formato para hora_inicio_tarde" );
+        $this->assertEqual( $this->DiaDisponibilidad->saveField( 'hora_fin_tarde', 'sdoidsosio' ), false, "No anduvo la regla de validacion de formato para hora_fin_tarde" );
+    }
 
 }
