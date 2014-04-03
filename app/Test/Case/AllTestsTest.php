394f46e9894c0d7db3d0360d757bfc0f79464463
tranfuga25s <tranfuga25s@users.noreply.github.com>
2014-04-03 14:47:56 -0300

    Update AllTestsTest.php
    Eliminados elementos no usados de cobertura de codigo.

--- app/Test/Case/AllTestsTest.php
+++ app/Test/Case/AllTestsTest.php
@@ -3,6 +3,8 @@
  * Clase para ejecutar todos los test
  */
 class AllTests extends PHPUnit_Framework_TestSuite {
+	
+      protected $coverageSetup = false;
 
     /**
      * Suite define the tests for this suite
@@ -10,11 +12,30 @@ class AllTests extends PHPUnit_Framework_TestSuite {
      * @return $suite
      */
 	public static function suite() {
-		$suite = new PHPUnit_Framework_TestSuite('All Tests');
-        $path = APP_TEST_CASES . DS;
-        //$suite->addTestFile( $path.'Controller'.DS.'ControllersTest.php' );
-        $suite->addTestFile( $path.'Model'.DS.'AllModelsTest.php' );
-        //$suite->addTestFile( $path.'View'.DS.'AllViewsTest.php');
+		$suite = new self('All Tests');
+        	$path = APP_TEST_CASES . DS;
+        	//$suite->addTestFile( $path.'Controller'.DS.'ControllersTest.php' );
+        	$suite->addTestFile( $path.'Model'.DS.'AllModelsTest.php' );
+        	//$suite->addTestFile( $path.'View'.DS.'AllViewsTest.php');
 		return $suite;
+	}
+	
+	
+	public function run(PHPUnit_Framework_TestResult $result = NULL, $filter = FALSE, array $groups = array(), array $excludeGroups = array(), $processIsolation = FALSE) {
+		if ($result === NULL) {
+			$result = $this->createResult();	
+		}
+		if (!$this->coverageSetup) {
+			$coverage = $result->getCodeCoverage();
+			if ($coverage) { // If the CodeCoverage is not installed or disabled
+				$coverage->setProcessUncoveredFilesFromWhitelist(true);
+			 
+				$coverageFilter = $coverage->filter();
+				$coverageFilter->addDirectoryToBlacklist( APP . DS . 'Test' );
+				$coverageFilter->addDirectoryToBlacklist( CORE_PATH );		
+			}
+			$this->coverageSetup = true;
+		}
+		return parent::run($result, $filter, $groups, $excludeGroups, $processIsolation);
 	}
 }
